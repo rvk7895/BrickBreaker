ddc236023c70e2d7b80fe657ec05935caeee7cd9
rvk7895 <rkal2009@gmail.com>
2021-02-10 15:13:23 +0530

    Canvas Made

--- config.py
+++ config.py
@@ -1,3 +1,21 @@
-LEFT_PADDING = ' '*20
-WIDTH = 150
-HEIGHT = 35
+from colorama import Fore
+from pyfiglet import figlet_format
+
+LEFT_PADDING = ' '*13
+WIDTH = 187
+HEIGHT = 45
+COLORS = [Fore.RED, 
+        Fore.GREEN, 
+        Fore.YELLOW, 
+        Fore.BLUE, 
+        Fore.MAGENTA, 
+        Fore.CYAN, 
+        Fore.WHITE]
+
+HEADER_TEXT = figlet_format("Brick Breaker")
+SPACES = ' '*77
+HEADER = SPACES
+for idx in range(len(HEADER_TEXT)):
+    HEADER += HEADER_TEXT[idx]
+    if(HEADER_TEXT[idx] == '\n'):
+        HEADER += SPACES
--- game.py
+++ game.py
@@ -2,25 +2,38 @@ import numpy as np
 import colorama as cl
 from colorama import Fore, Back, Style
 import config as cfg
-from art import tprint
 import os
 from time import sleep
 
 cl.init()
+idx = 0
 
-# tprint(cfg.LEFT_PADDING + ' '*40 + 'Brick Breaker')
-# # PRINTING THE FRAME
-# print(cfg.LEFT_PADDING + '_'*cfg.WIDTH)
-# for x in range(cfg.HEIGHT):
-#     print(cfg.LEFT_PADDING+'|'+' '*(cfg.WIDTH-2)+'|')
-# print(cfg.LEFT_PADDING+'_'*cfg.WIDTH)
+# INITIALIZING THE CANVAS
+CANVAS = [[' ']*cfg.WIDTH for _ in range(cfg.HEIGHT)]
+
+# INITIALIZING THE FRAME IN CANVAS
+CANVAS[0][0] = '╔'
+CANVAS[0][cfg.WIDTH-1] = '╗'
+CANVAS[cfg.HEIGHT-1][0] = '╚'
+CANVAS[cfg.HEIGHT-1][cfg.WIDTH-1] = '╝'
+for x in range(1,cfg.WIDTH-1):
+    CANVAS[0][x] = '═'
+    CANVAS[cfg.HEIGHT-1][x] = '═'
+for x in range(1,cfg.HEIGHT-1):
+    CANVAS[x][0] = '║'
+    CANVAS[x][cfg.WIDTH-1] = '║'
 
 while True:
     os.system('clear')
-    tprint(cfg.LEFT_PADDING + ' '*40 + 'Brick Breaker')
-    # PRINTING THE FRAME
-    print(cfg.LEFT_PADDING + '_'*cfg.WIDTH)
-    for x in range(cfg.HEIGHT):
-        print(cfg.LEFT_PADDING+'|'+' '*(cfg.WIDTH-2)+'|')
-    print(cfg.LEFT_PADDING+'_'*cfg.WIDTH)
-    sleep(1)
+    
+    # PRINTING THE HEADER
+    print(cfg.COLORS[idx] + cfg.HEADER + Fore.RESET)
+    idx = (idx + 1) % 7
+
+    # PRINTING THE CANVAS
+    for i in range(cfg.HEIGHT):
+        print(cfg.LEFT_PADDING,end="")
+        for j in range(cfg.WIDTH):
+            print(CANVAS[i][j],end="")
+        print()
+    sleep(0.1)
